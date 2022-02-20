32ad9edb3f33061a58c79c60abf31768beffda81
ggabdoll <ggabdoll0327@naver.com>
2022-02-20 22:59:57 +0900

    zero start

--- src/main/java/Zero.java
+++ src/main/java/Zero.java
@@ -1,6 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Zero {
 
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws Exception {
+
+
+        // 자 제로 문제 풀어보자~!
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
     }
 }
